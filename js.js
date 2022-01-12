461dc8cd62c3e0a4b90d327b9bfc6ed8690ce86b
Kungfu-Chsia <84034259+Kungfu-Chsia@users.noreply.github.com>
2022-01-12 13:09:43 +0300

    Add files via upload
    add js-slide

---
+++ js.js
@@ -0,0 +1,22 @@
+const tabs = document.querySelector(".slide-wrapper");
+const tabButton = document.querySelectorAll(".js-btn");
+const contents = document.querySelectorAll(".slider-item");
+
+tabs.onclick = e => {
+  const id = e.target.dataset.id;
+  if (id) {
+    tabButton.forEach(btn => {
+
+      btn.classList.remove("current");
+    });
+    e.target.classList.add("current");
+
+    contents.forEach(content => {
+    content.classList.remove("slide-current");
+    });
+
+    const element = document.getElementById(id);
+    console.log(element)
+    element.classList.add("slide-current");
+  }
+}
